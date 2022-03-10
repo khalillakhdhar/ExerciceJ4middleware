c43133ae60bc5f3cc3e0416bb9f11446098622bb
khalil <khalillakhdharatc@gmail.com>
2022-03-10 10:19:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ExerciceJ4middleware
+ 
---
+++ src/main/java/com/duranco/ExerciceJ4middlewareApplication.java
@@ -0,0 +1,13 @@
+package com.duranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciceJ4middlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciceJ4middlewareApplication.class, args);
+	}
+
+}
